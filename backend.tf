f9715117b7d687f41baa951378dd2c55ed905347
srinu856 <linga.srinu85@gmail.com>
2025-03-06 19:52:36 +0530

    hi

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "emrcicdpr2025" # Replace with your actual S3 bucket name
+    bucket = "devopscloud34pm" # Replace with your actual S3 bucket name
     key    = "Eks/terraform.tfstate"
-    region = "us-east-1"
+    region = "ap-south-1"
   }
 }
