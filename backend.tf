af38e4f23deef343fe395682f81b38ba42f751ff
srinu856 <linga.srinu85@gmail.com>
2025-03-16 08:19:02 +0530

    change code

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "devopscloud34pm" # Replace with your actual S3 bucket name
+    bucket = "bjnncbcxnbcjcd" # Replace with your actual S3 bucket name
     key    = "Eks/terraform.tfstate"
     region = "ap-south-1"
   }
